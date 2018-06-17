4ffcb8674ddb13c1089d305ea764470a36ecb27e
Eliran Bivas <bivas@users.noreply.github.com>
2018-06-17 14:23:13 +0200

    Helm presto (#2)

--- services/presto/docker/docker-presto.sh
+++ services/presto/docker/docker-presto.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
 set -e
 
+if [ "$#" != "1" ]; then
+  echo "ERROR: Missing required arguments"
+  echo "Expecting ${0} --config=<config path>"
+  echo "Got: $@"
+  exit 1
+fi
+
+for i in "$@"
+do
+case $i in
+    -c=*|--config=*)
+    CONFIG_PATH="${i#*=}"
+    echo "ConfigPath is $CONFIG_PATH"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+cp $CONFIG_PATH/*.properties $PRESTO_CONF_DIR
+cp $CONFIG_PATH/*.config $PRESTO_CONF_DIR
+
 launcher run
